e27a2296b7af6333b533c6b1b9229351314f4fd1
Elvis Cai <elvis@Elviss-MacBook-Pro.local>
2022-03-14 10:08:34 -0700

    added data retrieving process

--- proc_dat.R
+++ proc_dat.R
@@ -1,7 +1,17 @@
+library(stringr)
+
 csv_files <- list.files(path="updates", full.name=TRUE)
 
 # function track changes of one date # example track changes of day 2022-01-10
 
-track_date <- function(date){
-  
-}
+
+
+# Read date list:
+date_list <- read.table("intermediate/commits.csv", header=TRUE, sep="")
+date_list <- sort(date_list[,1])
+
+# Read last file:
+
+last_idx <- length(date_list)
+df_last<- read.table(csv_files[last_idx], header=TRUE, sep=",")
+df_last<- df_last[]
